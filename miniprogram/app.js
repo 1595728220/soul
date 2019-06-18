7acd92918bf02ae97d27829bac4f8164af83bf53
1595728220 <1595728220@qq.com>
2019-06-18 11:51:21 +0800

    调用login云函数获取openId

--- miniprogram/app.js
+++ miniprogram/app.js
@@ -1,7 +1,7 @@
 //app.js
 App({
-  onLaunch: function () {
-    
+  onLaunch: function() {
+
     if (!wx.cloud) {
       console.error('请使用 2.2.3 或以上的基础库以使用云能力')
     } else {
@@ -10,6 +10,14 @@ App({
       })
     }
 
-    this.globalData = {}
+    this.globalData = {
+      openId: null
+    }
+    wx.cloud.callFunction({
+      name: "login"
+    }).then(res => {
+      this.globalData.openId = res.result.openid
+      // console.log(this.globalData.openId)
+    }).catch(err => console.log(err))
   }
 })
